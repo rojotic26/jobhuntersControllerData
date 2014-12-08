d70d4bffb34d220d965cd66dd27e997aa9831da1
Edwin Mejia <eddwin@live.com>
2014-12-08 23:36:48 +0800

    more housekeeping

--- app.rb
+++ app.rb
@@ -1,61 +1,49 @@
 require 'sinatra/base'
-#require 'sinatra/namespace'
 require 'jobhunters'
 require 'json'
 require_relative 'model/offer'
-require 'httparty'
-require 'haml'
-require 'sinatra/flash'
 
-class TecolocoJobOffers < Sinatra::Base
-  #register Sinatra::Namespace
-  enable :sessions
-  register Sinatra::Flash
-  use Rack::MethodOverride
+class HuntingForAJobService < Sinatra::Base
+
   configure :production, :development do
     enable :logging
   end
 
-  API_BASE_URI = 'http://localhost:9292'
-  after { ActiveRecord::Base.connection.close }
   helpers do
+
     def offerobject
       category = check_cat(params[:category])
-       if category == 'none' then
-
-         return nil
-
-       end
-
-        return nil unless category
-        catego = { 'id' => category , 'offers' => [], }
-
-        begin
-          JobSearch::Tecoloco.getjobs(category).each do |title, date, cities, details|
-            catego['offers'].push('title'=>title,'date'=>date,'city'=>cities, 'details'=>details)
-          end
-          catego
-        rescue
-          nil
-        end
+      if category == 'none' then
+        return nil
       end
-      def get_jobs(category)
-        jobs_after = {
-          'type of job' => category,
-          'kind' => 'openings',
-          'jobs' => []
-        }
-
-        category = params[:category]
+      return nil unless category
+      catego = { 'id' => category , 'offers' => [], }
+      begin
         JobSearch::Tecoloco.getjobs(category).each do |title, date, cities, details|
-          jobs_after['jobs'].push('id' => title, 'date' => date, 'city' => cities, 'details'=>details)
+          catego['offers'].push('title'=>title,'date'=>date,'city'=>cities, 'details'=>details)
         end
-        jobs_after
+        catego
+      rescue
+        nil
+      end
+    end
 
+    def get_jobs(category)
+      jobs_after = {
+        'type of job' => category,
+        'kind' => 'openings',
+        'jobs' => []
+      }
+      category = params[:category]
+      JobSearch::Tecoloco.getjobs(category).each do |title, date, cities, details|
+        jobs_after['jobs'].push('id' => title, 'date' => date, 'city' => cities, 'details'=>details)
+      end
+      jobs_after
     end
 
 
     #Defining the function get_jobs_cat_city
+
     def get_jobs_cat_city(category,city)
       jobs_after_city = {
         'jobs' => []
@@ -63,20 +51,17 @@ class TecolocoJobOffers < Sinatra::Base
       flag=false
       cat = check_cat(category[0])
       cit = city[0]
-
-        JobSearch::Tecoloco.getjobs(cat).each do |title, date, cities, details|
-          if cities.to_s == cit.to_s
-            flag=true
-            jobs_after_city['jobs'].push('id' => title, 'date' => date, 'cities' => cities, 'details' => details)
-          end
+      JobSearch::Tecoloco.getjobs(cat).each do |title, date, cities, details|
+        if cities.to_s == cit.to_s
+          flag=true
+          jobs_after_city['jobs'].push('id' => title, 'date' => date, 'cities' => cities, 'details' => details)
         end
-        if flag==false then
-          halt 404
-        else
-          jobs_after_city
-        end
-
-
+      end
+      if flag==false then
+        halt 404
+      else
+        jobs_after_city
+      end
     end
 
     def get_jobs_cat_city_url(category,city)
@@ -89,28 +74,21 @@ class TecolocoJobOffers < Sinatra::Base
       flag=false
       category = params[:category]
       city = params[:city]
-
-
-        JobSearch::Tecoloco.getjobs(cat).each do |title, date, cities|
-          if cities.to_s == city.to_s
-            flag=true
-            jobs_after_city['jobs'].push('id' => title, 'date' => date)
-          end
+      JobSearch::Tecoloco.getjobs(cat).each do |title, date, cities|
+        if cities.to_s == city.to_s
+          flag=true
+          jobs_after_city['jobs'].push('id' => title, 'date' => date)
         end
-        if flag==false then
-          halt 404
-        else
-          jobs_after_city
-        end
-
-
-
+      end
+      if flag==false then
+        halt 404
+      else
+        jobs_after_city
+      end
     end
 
-    #Defining the function get_jobs_city
+    ##Checks if Category exists within Tecoloco
     def check_cat(category)
-      ##Checks if Category exists within Tecoloco
-
       case category
       when  "marketing"
         @output = "marketing-ventas"
@@ -130,18 +108,13 @@ class TecolocoJobOffers < Sinatra::Base
       end
       @list_all
     end
-
-    def current_page?(path = ' ')
-      path_info = request.path_info
-      path_info += ' ' if path_info == '/'
-      request_path = path_info.split '/'
-      request_path[1] == path
-    end
-
-
   end
 
-  get '/api/v1/job_openings/:category.json' do
+  get '/' do
+    ''
+  end
+
+  get '/api/v2/job_openings/:category.json' do
     cat = params[:category]
     category_url = check_cat(cat)
     if category_url == "none" then
@@ -153,12 +126,12 @@ class TecolocoJobOffers < Sinatra::Base
 
   end
 
-  get '/api/v1/job_openings/:category/city/:city.json' do
+  get '/api/v2/job_openings/:category/city/:city.json' do
     content_type :json
     get_jobs_cat_city_url(params[:category],params[:city]).to_json
   end
 
-  post '/api/v1/joboffers' do
+  post '/api/v2/joboffers' do
     content_type:json
 
     body = request.body.read
@@ -176,17 +149,17 @@ class TecolocoJobOffers < Sinatra::Base
     cat.city = req['city'].to_json
 
     if cat.save
-      redirect "/api/v1/offers/#{cat.id}"
+      redirect "/api/v2/offers/#{cat.id}"
     end
   end
 
-  delete '/api/v1/joboffers/:id' do
+  delete '/api/v2/joboffers/:id' do
     cat = Category.destroy(params[:id])
   end
 
-  get '/api/v1/offers/:id' do
+  get '/api/v2/offers/:id' do
     content_type:json
-    logger.info "GET /api/v1/offers/#{params[:id]}"
+    logger.info "GET /api/v2/offers/#{params[:id]}"
     begin
       @category = Category.find(params[:id])
       cat = JSON.parse(@category.category)
@@ -200,6 +173,4 @@ class TecolocoJobOffers < Sinatra::Base
     logger.info "result: #{result}\n"
     result
   end
-
-  
 end
