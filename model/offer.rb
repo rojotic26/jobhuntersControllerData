b134d7d3daed4c1bfa43e6c101c3342eac099da1
Edwin Mejia <eddwin@live.com>
2014-12-14 16:38:59 +0800

    Modified model with AWS stuff

--- model/offer.rb
+++ model/offer.rb
@@ -1,8 +1,15 @@
-require 'sinatra'
-require 'sinatra/activerecord'
-require_relative '../config/environments'
+require 'aws-sdk'
 
-class Offer < ActiveRecord::Base
+class Tutorial < AWS::Record::HashModel
+  string_attr :title
+  string_attr :date
+  string_attr :city
+  string_attr :details
+  timestamps
 end
 class Category < ActiveRecord::Base
+  string_attr :description
+  string_attr :category
+  string_attr :city
+  timestamps
 end
