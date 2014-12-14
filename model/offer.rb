9cd92a4f07969c8576a43e376c8ce0f41255bd5b
Edwin Mejia <eddwin@live.com>
2014-12-14 16:46:17 +0800

    added delete methods to db

--- model/offer.rb
+++ model/offer.rb
@@ -6,10 +6,28 @@ class Tutorial < AWS::Record::HashModel
   string_attr :city
   string_attr :details
   timestamps
+
+  def self.destroy(id)
+    find(id).delete
+  end
+
+  def self.delete_all
+    all.each { |r| r.delete }
+  end
+
 end
 class Category < ActiveRecord::Base
   string_attr :description
   string_attr :category
   string_attr :city
   timestamps
+
+  def self.destroy(id)
+    find(id).delete
+  end
+
+  def self.delete_all
+    all.each { |r| r.delete }
+  end
+  
 end
